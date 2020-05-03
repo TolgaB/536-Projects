c9df1b9c4559e2d9652902b3cf36a495c7de5aa6
Tolga Beser <mathman290@gmail.com>
2020-05-03 09:35:44 -0700

    getting wonky

--- p6 secondtry/ast.java
+++ p6 secondtry/ast.java
@@ -2752,13 +2752,23 @@ class AndNode extends LogicalExpNode {
     }
 
     public void codeGen() {
+        String failedLbl = Codegen.nextLabel();
+        String workedLbl = Codegen.nextLabel();
         myExp1.codeGen();
-        myExp2.codeGen();
         Codegen.genPop(Codegen.A0);
         Codegen.generate("move", Codegen.T0, Codegen.A0);
+        Codegen.generate("beqz", Codegen.T0, failedLbl);
+
+        myExp2.codeGen();
         Codegen.genPop(Codegen.A0);
         Codegen.generate("and", Codegen.A0, Codegen.A0, Codegen.T0);
         Codegen.genPush(Codegen.A0);
+        Codegen.generate("j", workedLbl);
+
+        //go here if fail
+        Codegen.genLabel(failedLbl);
+        Codegen.genPush(Codegen.T0);
+        Codegen.genLabel(workedLbl);
     }
 
 }
